27243c7d8603655c1289860e2e438cc7c69c4db1
Michelle Villaça Andry <michellevandry@gmail.com>
2026-04-17 21:52:34 -0300

    mudando os nomes das variáveis

--- conexao.php
+++ conexao.php
@@ -1,20 +1,23 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$database = "colonial_veiculos";
+$host = "localhost";
+$usuario = "root";
+$senha = "";
+$banco = "colonial_veiculos";
 
 try {
-    $conexao = new mysqli($servername, $username, $password, $database);
+    // Criando a conexão com o banco de dados
+    $conexao = new mysqli($host, $usuario, $senha, $banco);
     
+    // connect_error é uma propriedade do objeto mysqli que retorna uma string descrevendo o erro de conexão, ou uma string vazia se não houver erro. Se a conexão falhar, lançamos uma exceção com a mensagem de erro.
     if ($conexao->connect_error) {
         throw new Exception("Conexão falhou: " . $conexao->connect_error);
     }
     
     $conexao->set_charset("utf8mb4");
     
-} catch (Exception $e) {
-    echo "Erro: " . $e->getMessage();
+} catch (Exception $erro) {
+    // Se ocorrer um erro, exibimos a mensagem de erro e encerramos o script
+    echo "Erro detectado: " . $erro->getMessage();
     exit();
 }
 ?>

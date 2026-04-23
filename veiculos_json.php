6ca738b0ca5667438ef9146aa051561fc5b79c2e
Michelle Villaça Andry <michellevandry@gmail.com>
2026-04-22 22:17:12 -0300

    Criação da tabela de veículos

---
+++ veiculos_json.php
@@ -0,0 +1,21 @@
+<?php
+include_once("conexao.php");
+
+try {
+    $sql = "SELECT * FROM veiculos ORDER BY data_cadastro DESC";
+    $resultado = $conexao->query($sql);
+    
+    $veiculos = [];
+    while($linha = $resultado->fetch_assoc()) {
+        $veiculos[] = $linha;
+    }
+
+    // Define que o navegador deve ler isso como JSON
+    header('Content-Type: application/json');
+    echo json_encode($veiculos);
+
+} catch (Exception $erro) {
+    echo json_encode(["erro" => $erro->getMessage()]);
+}
+$conexao->close();
+?>
